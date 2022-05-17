9724f94e4810dd2668a22e0e9e633f450e00b5d9
BurritoFreak <chris.sarmousakis@gmail.com>
2022-05-17 13:33:23 -0400

    main

--- src/components/Character.js
+++ src/components/Character.js
@@ -27,14 +27,12 @@ const Character = ({ character, refreshCharacters }) => {
     
     return (
         <div class="box">
-        <a href = "/characters">
             <h2>{character.Player}</h2>
             <div className="character-card">
                 <div className="character-card-body">
                     <h3>{character.Name}</h3>
                 </div>
             </div>
-            </a>
         </div>
     )
 };
