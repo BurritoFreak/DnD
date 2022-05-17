94a212adf8c0c3719ee1fd0e6167f88447b9904b
BurritoFreak <chris.sarmousakis@gmail.com>
2022-05-17 15:55:45 -0400

    main

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,8 +1,10 @@
 import React from "react";
 import Character from "./Character";
+import CharacterSheet from "./CharacterSheet";
 
 const CharacterList = ({ characters, refreshCharacters }) => {
     return (
+        <>
         <div className="characters">
         {characters.map((character) => 
             <Character
@@ -12,7 +14,13 @@ const CharacterList = ({ characters, refreshCharacters }) => {
             />
         )}
         </div>
-    );
+        <div className="character-sheets">
+        {characters.map((character) => 
+            <CharacterSheet character={character} />
+        )}
+        </div>
+        </>
+        );
 };
 
 export default CharacterList;
