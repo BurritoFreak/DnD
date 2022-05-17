52c5b72a5c2c3ab7035a1ae86e336534dbdbbf30
BurritoFreak <chris.sarmousakis@gmail.com>
2022-05-17 17:03:39 -0400

    main

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import "./App.css";
 import CharacterList from "./components/CharacterList";
 import CharacterSheet from "./components/CharacterSheet";
 
-
+/*
 function App() {
     const [characters, setCharacters] = useState([]);
 
@@ -30,11 +30,52 @@ function App() {
         </div>
     );
 }
-
+*/
 //{characters.map((character) => 
 //    <Route path={"/:character"}>
 //        <CharacterSheet character={character} />
 //    </Route>
 //)}
 
-export default App;
+//export default App;
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleButtonClick = this.handleButtonClick.bind(this);
+        this.state = {
+            characters: [],
+            setCharacters: []
+        };
+    }
+
+    handleLoginClick() {
+        this.setState({isLoggedIn: true});
+    }
+
+    render () {
+        const [characters, setCharacters] = this.useState([]);
+
+        const loadCharacters = async () => {
+        try {
+            const res = await fetch('/.netlify/functions/characters');
+            const characters = await res.json();
+            setCharacters(characters);
+        } catch (error) {
+            console.log(error);
+        }
+        };
+
+        this.useEffect(() => {
+            loadCharacters();
+        }, []);
+
+        return (
+            <div className="App">
+                <CharacterList characters={characters} refreshCharacters={loadCharacters}/>
+                <CharacterSheet character={characters[0]} />
+            </div>
+        );
+    }
+    
+}
